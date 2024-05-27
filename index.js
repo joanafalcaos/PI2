01f6ce70cab0c30ae71d565fd9e5c33a99588125
joycebarbosag <joycebarbosa.gomess@gmail.com>
2024-05-27 17:44:36 -0300

    Subindo primeira extração de dados do back4app

--- index.js
+++ index.js
@@ -100,13 +100,50 @@ var chartSeis = new Chart(ctx, {
     }
   })
 
-const url = "https://parseapi.back4app.com/classes/BaseEnade";
-const headers = {
-  "X-Parse-Application-Id": "ctoAeEYlkHYxqP80X1fVEwtFshGtMdzpnJOg9vcK",
-  "X-Parse-REST-API-Key": "4xenXpwAwrWzTbARXn37UQ5wpcj0hCuLHYMFThis",
-};
-const headersJson = {
-  ...headers,
-  "Content-Type": "application/json",
+  const censoPeClassUrl = "https://parseapi.back4app.com/classes/CensoPeClass";
+  const headers = {
+    "X-Parse-Application-Id": "zS4HA9IMZEWTlTnnx4C0pDpFssKbp8LM9aNxkhY8",
+    "X-Parse-REST-API-Key": "1A7kwTiWyeg1GwXDl8rcoyStzWJB4SId5PX8wGOh",
+  };
+  const headersJson = {
+    ...headers,
+    "Content-Type": "application/json",
+  };
+
+  const getQtAnyLibrarie = async (anyLibrarie) => {
+    let url = censoPeClassUrl;
+    if(anyLibrarie){
+        const whereClause = JSON.stringify({
+            IN_BIBLIOTECA: {
+              $in: [0, undefined]
+            }
+          });
+        url = `${url}?where=${whereClause}`;
+        url = encodeURI(url);
+        console.log("url", url);
+    }
+    const response = await fetch(url, {
+        method: "GET",
+        headers: headers,
+      });
+      const data = await response.json();
+      return data.results;
+  };
+
+const showQtAnyLibrarie = async () => {
+  //Chamando a func para pegar os resultados
+  const escolasComBiblioteca = await getQtAnyLibrarie(true);
+
+  //limpando dados antigos
+  resultadoContainer.innerHTML = "";
+
+  // Iterando para contar a qt
+  var count = 0;
+  escolasComBiblioteca.forEach(escola => {
+    count++;
+  });
+    var anyLibrariesQt = document.getElementById('total-schools-without-libraries')
+    anyLibrariesQt.textContent = count;
 };
 
+showQtAnyLibrarie();
