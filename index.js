11c123cc15ef15d646fec75fc3d2d544174130c7
joycebarbosag <joycebarbosa.gomess@gmail.com>
2024-05-28 17:15:53 -0300

    Ajustando html, css e js

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-var ctx = document.getElementsByClassName("total-libraries-states")
+const ctx = document.getElementsByClassName("school-type-adm");
 
-var chartUm = new Chart(ctx, {
+const data = {
+  labels: ["Municipal", "Estadual", "Federal"],
+  datasets: [{
+    label: "Quantidade",
+    data: [5,10,2],
+    borderWidth: 3,
+    borderColor: 'rgba(77,166,253,0.85)',
+    backgroundColor: 'transparent',
+  }]
+};
+
+const config = {
+  type: 'pie',
+  data: {
+    labels: data.labels,
+    datasets: [{
+      label: data.datasets[0].label,
+      data: data.datasets[0].data,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.5)',
+        'rgba(255, 206, 86, 0.5)',
+      ]
+    }]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Pie Chart'
+      }
+    }
+  }
+};
+
+const chartUm = new Chart(ctx, config);
+
+var ctx2 = document.getElementsByClassName("total-libraries-region")
+
+var chartDois = new Chart(ctx2, {
     type:'line',
     data:{
       labels: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
@@ -15,9 +58,9 @@ var chartUm = new Chart(ctx, {
     }
   })
 
-var ctx = document.getElementsByClassName("total-libraries-region")
+var ctx3 = document.getElementsByClassName("total-public-libraries-state")
 
-var chartDois = new Chart(ctx, {
+var chartTres = new Chart(ctx3, {
     type:'line',
     data:{
       labels: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
@@ -32,9 +75,9 @@ var chartDois = new Chart(ctx, {
     }
   })
 
-var ctx = document.getElementsByClassName("total-public-libraries-state")
+var ctx4 = document.getElementsByClassName("total-public-libraries-region")
 
-var chartTres = new Chart(ctx, {
+var chartQuatro = new Chart(ctx4, {
     type:'line',
     data:{
       labels: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
@@ -49,43 +92,9 @@ var chartTres = new Chart(ctx, {
     }
   })
 
-var ctx = document.getElementsByClassName("total-public-libraries-region")
+var ctx5 = document.getElementsByClassName("total-private-libraries-state")
 
-var chartQuatro = new Chart(ctx, {
-    type:'line',
-    data:{
-      labels: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
-      datasets: [{
-        label: "Quantidade",
-        data: [5,10,5,14,20,15,6,14,8,12,15,5,10,5,10,5,14,20,15,6,14,8,12,15,5,10,12],
-        borderWidth: 3,
-        borderColor: 'rgba(77,166,253,0.85)',
-        backgroundColor: 'transparent',
-      }]
-
-    }
-  })
-
-var ctx = document.getElementsByClassName("total-private-libraries-state")
-
-var chartCinco = new Chart(ctx, {
-    type:'line',
-    data:{
-      labels: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
-      datasets: [{
-        label: "Quantidade",
-        data: [5,10,5,14,20,15,6,14,8,12,15,5,10,5,10,5,14,20,15,6,14,8,12,15,5,10,12],
-        borderWidth: 3,
-        borderColor: 'rgba(77,166,253,0.85)',
-        backgroundColor: 'transparent',
-      }]
-
-    }
-  })
-
-var ctx = document.getElementsByClassName("total-private-libraries-region")
-
-var chartSeis = new Chart(ctx, {
+var chartCinco = new Chart(ctx5, {
     type:'line',
     data:{
       labels: ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"],
@@ -110,13 +119,12 @@ var chartSeis = new Chart(ctx, {
     "Content-Type": "application/json",
   };
 
+  // BOX DE ESCOLAS SEM BIBLIOTECAS
   const getQtAnyLibrarie = async (anyLibrarie) => {
     let url = censoPeClassUrl;
     if(anyLibrarie){
         const whereClause = JSON.stringify({
-            IN_BIBLIOTECA: {
-              $in: [0, undefined]
-            }
+            IN_BIBLIOTECA: 0
           });
         url = `${url}?where=${whereClause}`;
         url = encodeURI(url);
@@ -140,6 +148,7 @@ const showQtAnyLibrarie = async () => {
   // Iterando para contar a qt
   var count = 0;
   escolasComBiblioteca.forEach(escola => {
+    console.log(escola)
     count++;
   });
     var anyLibrariesQt = document.getElementById('total-schools-without-libraries')
@@ -147,3 +156,84 @@ const showQtAnyLibrarie = async () => {
 };
 
 showQtAnyLibrarie();
+
+/// onclick func
+function change(buttonId) {
+  var img = document.getElementById('map');
+  switch(buttonId) {
+      case 'general':
+          img.src = 'img/rmr_mapa.png';
+          img.alt = 'Mapa da RMR de Recife';
+          img.title = 'Mapa da RMR de Recife';
+          break;
+      case 'abreu':
+          img.src = 'img/mapa_abreu_lima.png'; // Substitua pelo caminho da outra imagem
+          img.alt = 'Abreu e Lima'; // Substitua pela descrição da outra imagem
+          img.title = 'Abreu e Lima'; // Substitua pelo título da outra imagem
+          break;
+      case 'cabo':
+          img.src = 'img/cabo_sto_agostinho_mapa.png';
+          img.alt = 'Cabo de Santo Agostinho';
+          img.title = 'Cabo de Santo Agostinho';
+          break;
+      case 'camaragibe':
+          img.src = 'img/camaragibe_mapa.png';
+          img.alt = 'Camaragibe';
+          img.title = 'Camaragibe';
+          break;
+      case 'goiana':
+          img.src = 'img/goiana_mapa.png';
+          img.alt = 'Goiana';
+          img.title = 'Goiana';
+          break;
+      case 'igarassu':
+          img.src = 'img/igarassu_mapa.png';
+          img.alt = 'Igarassu';
+          img.title = 'Igarassu';
+          break;
+      case 'itamaraca':
+          img.src = 'img/ilha_itamaraca_mapa.png';
+          img.alt = 'Ilha de Itamaracá';
+          img.title = 'Ilha de Itamaracá';
+          break;
+      case 'ipojuca':
+          img.src = 'img/ipojuca_mapa.png';
+          img.alt = 'Ipojuca';
+          img.title = 'Ipojuca';
+          break;
+      case 'itapissuma':
+          img.src = 'img/itapissuma_mapa.png';
+          img.alt = 'Itapissuma';
+          img.title = 'Itapissuma';
+          break;
+      case 'jaboatao':
+          img.src = 'img/jaboatao_guararapes_mapa.png';
+          img.alt = 'Jaboatão dos Guararapes';
+          img.title = 'Jaboatão dos Guararapes';
+          break;
+      case 'moreno':
+          img.src = 'img/moreno_mapa.png';
+          img.alt = 'Moreno';
+          img.title = 'Moreno';
+          break;
+      case 'olinda':
+          img.src = 'img/olinda_mapa.png';
+          img.alt = 'Olinda';
+          img.title = 'Olinda';
+          break;
+      case 'paulista':
+          img.src = 'img/paulista_mapa.png';
+          img.alt = 'Paulista';
+          img.title = 'Paulista';
+          break;
+      case 'recife':
+          img.src = 'img/recife_mapa.png';
+          img.alt = 'Recife';
+          img.title = 'Recife';
+          break;
+      case 'slourenco':
+          img.src = 'img/sao_lourenco_mapa.png';
+          img.alt = 'São Lourenço da Mata';
+          img.title = 'São Lourenço da Mata';
+  }
+}
